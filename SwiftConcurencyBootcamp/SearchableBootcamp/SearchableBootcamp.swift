267835b23475fbceda2cba25784ad4583c7f14f0
Serhan Khan <khanboy0689@gmail.com>
2024-09-24 22:30:41 +0300

    impl start: SearchableBootcamp

---
+++ SwiftConcurencyBootcamp/SearchableBootcamp/SearchableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SearchableBootcamp.swift
+//  SwiftConcurencyBootcamp
+//
+//  Created by Serhan Khan on 24/09/2024.
+//
+
+import SwiftUI
+
+struct SearchableBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchableBootcamp()
+}
