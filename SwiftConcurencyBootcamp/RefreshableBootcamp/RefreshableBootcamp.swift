13c99ba177348096327b2ef35075eee871ccd36c
Serhan Khan <khanboy0689@gmail.com>
2024-09-24 22:08:38 +0300

    impl: Refreshable View Modifier

---
+++ SwiftConcurencyBootcamp/RefreshableBootcamp/RefreshableBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  RefreshableBootcamp.swift
+//  SwiftConcurencyBootcamp
+//
+//  Created by Serhan Khan on 24/09/2024.
+//
+
+import SwiftUI
+
+final class RefreshableDataService {
+    func getData() async throws -> [String] {
+        try? await Task.sleep(nanoseconds: 5_000_000_000)
+        return ["Apple", "Orange", "Banana"].shuffled()
+    }
+}
+
+@MainActor
+final class RefreshableBootcampViewModel: ObservableObject {
+    let manager = RefreshableDataService()
+    @Published private(set) var items: [String] = []
+    
+    func loadData() async {
+        do {
+            items = try await self.manager.getData()
+        } catch {
+            print(error)
+        }
+    }
+}
+
+struct RefreshableBootcamp: View {
+    @StateObject private var viewModel = RefreshableBootcampViewModel()
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    ForEach(viewModel.items, id: \.self) {
+                        Text($0)
+                            .font(.headline)
+                    }
+                }
+            }.refreshable {
+                await viewModel.loadData()
+            }
+            .navigationTitle("Refreshable")
+            .task {
+                await viewModel.loadData()
+            }
+        }
+    }
+}
+
+#Preview {
+    RefreshableBootcamp()
+}
